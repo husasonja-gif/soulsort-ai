6a308ed7674ec1cf097059366667525a6926af2a
husasonja-gif <husasonja@gmail.com>
2026-04-01 14:03:50 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/edge/beta/:path*",
+        destination: "https://soulsort-edge.vercel.app/edge/beta/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
