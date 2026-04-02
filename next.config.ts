3e94db6aecc7eb64c165fe4029cdf9dcee505a68
husasonja-gif <husasonja@gmail.com>
2026-04-02 12:11:49 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/edge/beta",
+        destination: "https://soulsort-edge.vercel.app/edge/beta",
+      },
+      {
+        source: "/edge/beta/",
+        destination: "https://soulsort-edge.vercel.app/edge/beta/",
+      },
+      {
         source: "/edge/beta/:path*",
         destination: "https://soulsort-edge.vercel.app/edge/beta/:path*",
       },
